28d345db4b98c064f0d687a5b484c59a68017be1
Priyansh <priyanshkh14@gmail.com>
2025-02-20 00:18:32 +0530

    changes : completed routing setup and clerk auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+import PublicLayout from "@/layouts/public-layout"
+import AuthenticationLayout from "@/layouts/auth-layout"
+import ProtectedRoutes from "@/layouts/protected-routes"
+import MainLayout from "@/layouts/main-layout"
+
+import HomePage from "@/routes/home"
+import SignInPage from "@/routes/signin"
+import SignUpPage from "@/routes/signup"
 
 const App = () => {
   return (
-    <div className='text-blue-400'>App</div>
+    <Router>
+      <Routes>
+        {/* public routes */}
+        <Route element={<PublicLayout />}>
+          <Route index element={<HomePage />} />
+        </Route>
+
+        {/* authentication layout */}
+        <Route element={<AuthenticationLayout />}>
+          <Route path="/signin/*" element={<SignInPage />} />
+          <Route path="/signup/*" element={<SignUpPage />} />
+        </Route>
+
+        {/* protected routes */}
+        <Route element={<ProtectedRoutes><MainLayout /></ProtectedRoutes>} />
+
+        {/* all the protected routes*/}
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/layouts/public-layout.tsx
@@ -0,0 +1,16 @@
+import Footer from "@/components/footer"
+import Header from "@/components/header"
+import { Outlet } from "react-router-dom"
+
+const PublicLayout = () => {
+  return (
+    <div className="w-full">
+        {/* handler to store the user data */}
+        <Header />
+        <Outlet />
+        <Footer />
+    </div>
+  )
+}
+
+export default PublicLayout
